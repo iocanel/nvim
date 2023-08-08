76fa3005d9679e37343f2d1190c2b581576ac244
Ioannis Canellos <iocanel@gmail.com>
2023-08-08 13:40:18 +0300

    feat: add bindings for jumping to next window

--- lua/plugins/winpick.lua
+++ lua/plugins/winpick.lua
@@ -1,3 +1,22 @@
+local M = {
+  window_next = function()
+    local current_win = vim.fn.win_getid()
+    local all_wins = vim.fn.getwininfo()
+    local next_win = nil
+
+    for i, win in ipairs(all_wins) do
+      if win.winid == current_win then
+        next_win = all_wins[(i % #all_wins) + 1]
+        break
+      end
+    end
+
+    if next_win then
+      vim.fn.win_gotoid(next_win.winid)
+    end
+  end
+};
+
 return {
   { 
     'gbrlsnchs/winpick.nvim', commit = '044623e236750a2f61a2cb96ce0833e113921b88', 
@@ -8,7 +27,7 @@ return {
           local winpick = require('winpick')
           local winid = winpick.select()
           if winid then
-  	        vim.api.nvim_set_current_win(winid)
+            vim.api.nvim_set_current_win(winid)
           end
         end, 
         desc = 'window pick' 
@@ -23,7 +42,13 @@ return {
           end
         end, 
         desc = 'window kill' 
-      } 
+      },
+      { 
+        "<leader>wn", M.window_next, desc = 'window next' 
+      },
+      {
+        "<M-o>", M.window_next, desc = 'window next'
+      }
     }
   }
 }
