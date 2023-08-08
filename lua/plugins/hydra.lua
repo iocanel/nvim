6f9c9e7c39cdeac3fd3ae703e7f3825a5d26d4da
Ioannis Canellos <iocanel@gmail.com>
2023-08-08 15:46:33 +0300

    feat: add gitsigns hydra

---
+++ lua/plugins/hydra.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    'anuvyklack/hydra.nvim',
+    commit = '3ced42c0b6a6c85583ff0f221635a7f4c1ab0dd0', 
+    priority = 60 -- Use +10 so that it's loaded before regular plugin that have priority 50.
+  }
+}
