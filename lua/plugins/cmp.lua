082b603866f833266d7b5d6dfd111774263b5e7d
Ioannis Canellos <iocanel@gmail.com>
2023-09-14 09:27:28 +0300

    chore: unbind TAB from nvim-cmp for codeium to use

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -28,7 +28,7 @@ return {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           },
-          ['<Tab>'] = cmp.mapping(function(fallback)
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
@@ -36,15 +36,6 @@ return {
             else
               fallback()
             end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
         end, { 'i', 's' }),
       },
       sources = {
