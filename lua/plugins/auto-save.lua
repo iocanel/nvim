f9040e004dc8ce9427237eb8d40b31aa3303d728
Ioannis Canellos <iocanel@gmail.com>
2023-10-23 09:26:54 +0300

    feat: add autosave plugin

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'Pocco81/auto-save.nvim',
+    opts = {
+      enabled = true,
+      trigger_events = { "BufLeave", "WinLeave", "WinClose", "FocusLost" },
+      debounce_delay = 120,
+     	condition = function(buf)
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+        if
+          fn.getbufvar(buf, "&modifiable") == 1 and
+          utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+          return true -- met condition(s), can save
+		    end
+		    return false -- can't save
+	    end,
+      write_all_buffers = false,
+      execution_message = {
+        dim = 0.18,
+        cleaning_interval = 5000
+      }
+    }
+  }
+}
