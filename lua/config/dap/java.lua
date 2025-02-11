4afacedc0ffb620ae392fcbc8efa429f8f0a7e2f
Ioannis Canellos <iocanel@gmail.com>
2025-02-11 09:35:41 +0200

    chore: dap port management changes

--- lua/config/dap/java.lua
+++ lua/config/dap/java.lua
@@ -1,8 +1,14 @@
 local M = {
+  default_settings = {
+    debug_port = 5005,
+  },
+  settings = nil;
 };
 
 function M:attach_to_remote(port)
-  port = port or 5005
+  local project = require('config.project')
+  M.settings = project.load_project_settings('debug', M.default_settings)
+  port = port or M.settings.debug_port or 5005
   local dap = require('dap')
   dap.configurations.java = {
     {
