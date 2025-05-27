c0fa151443ab3be2476c108d40a6bf5f042a4916
Ioannis Canellos <iocanel@gmail.com>
2025-05-27 12:03:24 +0300

    fix: issues with copilot chat forgeteting context

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,12 +5,15 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = {
+      model = 'gpt-4.1',
+      agent = 'copilot',
       show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
       debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
       disable_extra_info = 'no', -- Disable extra information (e.g: system prompt) in the response.
-      language = "English" -- Copilot answer language settings when using default prompts. Default language is English.
+      language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
       -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
-      -- temperature = 0.1,
+      temperature = 0.1,
+      sticky = { "conversation" },
     },
     build = function()
       vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
