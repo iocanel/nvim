e77d233a71913c9caf68d66f426b19463a64b7ed
Ioannis Canellos <iocanel@gmail.com>
2025-07-09 16:07:19 +0300

    chore: remove mason commit sha from mason lazy config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,14 +1,12 @@
 return {
   {
     'williamboman/mason.nvim',
-    commit = 'fc98833b6da5de5a9c5b1446ac541577059555be',
     opts = {
     }
   },
   {
     'williamboman/mason-lspconfig.nvim',
     dependencies = { 'williamboman/mason.nvim' },
-    commit = '1a31f824b9cd5bc6f342fc29e9a53b60d74af245',
     opts = {
       ensure_installed = { 'html', 'cssls', 'jsonls', 'ts_ls', 'vue-langage-server', 'js-debug-adapter', 'lua_ls', 'rust_analyzer', 'gopls', 'ts_ls', 'pyright', 'solidity', 'intelephense', 'ltex' },
       automatic_installation = true,
