ceb60f70b1bc24e70e21471b75b618a5b88ead6a
Ioannis Canellos <iocanel@gmail.com>
2025-07-09 13:31:29 +0300

    chore: remove hydra fixed commit

--- lua/plugins/hydra.lua
+++ lua/plugins/hydra.lua
@@ -1,7 +1,6 @@
 return {
   {
     'nvimtools/hydra.nvim',
-    commit = '983852960cd4e4e9a1b272df0bd1447495aae755',
     priority = 60 -- Use +10 so that it's loaded before regular plugin that have priority 50.
   }
 }
