90c9857339c7b46ed867844fb3a2eae1ba83d65d
Ioannis Canellos <iocanel@gmail.com>
2025-10-15 10:19:25 +0300

    refactor: align javascript tests with luanit

---
+++ tests/javascript/config.lua
@@ -0,0 +1,32 @@
+-- JavaScript Language Test Configuration
+-- Defines JavaScript-specific test parameters and requirements
+
+local M = {}
+
+-- JavaScript LSP Configuration
+M.lsp = {
+  server_name = "ts_ls",
+  timeout_env_var = "TS_LS_WAIT_MS",
+  default_timeout = 20000,
+  expected_filetype = "javascript",
+  language = "JavaScript",
+  required_caps = {
+    "textDocumentSync"
+  },
+  optional_caps = {
+    "hoverProvider",
+    "definitionProvider", 
+    "referencesProvider",
+    "documentSymbolProvider",
+    "workspaceSymbolProvider"
+  },
+}
+
+-- JavaScript DAP Configuration
+M.dap = {
+  adapter_name = "pwa-node",
+  timeout_env_var = "DAP_WAIT_MS",
+  default_timeout = 20000,
+}
+
+return M
