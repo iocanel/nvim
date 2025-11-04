a391be60921fe497783fc7a4f97c46efb73db4b4
Ioannis Canellos <iocanel@gmail.com>
2025-11-04 09:08:53 +0200

    feat: introduce basic org-mode support

---
+++ lua/plugins/org-mode.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    'nvim-orgmode/orgmode',
+
+    config = function()
+      -- Setup orgmode
+      require('orgmode').setup({
+        org_agenda_files = {
+          "/home/iocanel/Documents/org/github.org",
+          "/home/iocanel/Documents/org/habits.org",
+          "/home/iocanel/Documents/org/nutrition.org",
+          "/home/iocanel/Documents/org/workout.org",
+          "/home/iocanel/Documents/org/calendars/personal.org",
+          "/home/iocanel/Documents/org/calendars/work.org",
+          "/home/iocanel/Documents/org/roam/Inbox.org",
+        },
+        org_default_notes_file = '~/Documents/org/roam/Inbox.org',
+      })
+    end,
+  },
+  -- Org bullets (prettier headings)
+  {
+    "akinsho/org-bullets.nvim",
+    ft = { "org" },
+    config = function()
+      require("org-bullets").setup({
+
+        symbols = {
+          -- list symbol
+          list = "•",
+          -- headlines can be a list
+          headlines = { "◉", "○", "✸", "✿" },
+        }
+      })
+    end,
+  }
+}
