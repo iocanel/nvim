347f319263afb715a4ced0147f6a5e3b7db87680
Ioannis Canellos <iocanel@gmail.com>
2026-01-11 09:24:45 +0200

    change(git): expose more funcionality via keymaps/whichkey/hydra

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -17,8 +17,10 @@ return {
       if is_hydra_installed then
         local git_hint = [[
         _n_: next hunk   _s_: stage hunk        _d_: show deleted   _b_: blame line
-        _p_: prev hunk   _r_: reset hunk        _v_: preview hunk   _B_: blame show full 
-        ^ ^              _u_: unstage hunk      ^ ^                 _/_: show base file
+        _p_: prev hunk   _r_: reset hunk        _v_: preview hunk   _B_: blame show full
+        ^ ^              _u_: unstage hunk      _w_: word diff      _l_: line highlight
+        ^ ^              _S_: stage buffer      _D_: diff this      _/_: show base file
+        ^ ^              _R_: reset buffer      ^ ^                 ^ ^
         ^
         ^ ^              _<Enter>_: Neogit              _q_: exit
         ]]
@@ -49,8 +51,12 @@ return {
             { 'r', "<cmd>Gitsigns reset_hunk<cr>", { silent = true, desc = 'reset hunk' } },
             { 'u', "<cmd>Gitsigns undo_stage_hunk<cr>", { desc = 'undo last stage' } },
             { 'S', "<cmd>Gitsigns stage_buffer<cr>", { desc = 'stage buffer' } },
+            { 'R', "<cmd>Gitsigns reset_buffer<cr>", { desc = 'reset buffer' } },
             { 'v', "<cmd>Gitsigns preview_hunk<cr>", { desc = 'preview hunk' } },
             { 'd', "<cmd>Gitsigns toggle_deleted<cr>", { nowait = true, desc = 'toggle deleted' } },
+            { 'D', "<cmd>Gitsigns diffthis<cr>", { desc = 'diff this' } },
+            { 'w', "<cmd>Gitsigns toggle_word_diff<cr>", { desc = 'toggle word diff' } },
+            { 'l', "<cmd>Gitsigns toggle_linehl<cr>", { desc = 'toggle line highlight' } },
             { 'b', "<cmd>Gitsigns blame_line<cr>", { desc = 'blame' } },
             { 'B', function() gitsigns.blame_line{ full = true } end, { desc = 'blame show full' } },
             { '/', "<cmd>Gitsigns show<cr>", { exit = true, desc = 'show base file' } }, -- show the base of the file
