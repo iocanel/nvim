4a7aac354c43f98b3c10083a5fe5f908194cdb64
Ioannis Canellos <iocanel@gmail.com>
2026-04-24 09:51:34 +0300

    refactor(plantuml): use local plantuml binary instead of Docker
    Remove Docker wrapper script and plantuml-previewer.vim since the
    previewer cannot call the plantuml binary directly. Keep
    aklt/plantuml-syntax which sets makeprg=plantuml for :make.

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -1,29 +1,6 @@
-local script_dir = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":h:h:h")
-local plantuml_script = script_dir .. "/scripts/plantuml-docker.sh"
-
 return {
   {
     "aklt/plantuml-syntax",
     ft = "plantuml",
-    init = function()
-      vim.g.plantuml_executable_script = plantuml_script
-    end,
-  },
-  {
-    "tyru/open-browser.vim",
-    lazy = true,
-  },
-  {
-    "weirongxu/plantuml-previewer.vim",
-    ft = "plantuml",
-    dependencies = {
-      "aklt/plantuml-syntax",
-      "tyru/open-browser.vim",
-    },
-    init = function()
-      vim.g["plantuml_previewer#java_path"] = plantuml_script
-      vim.g["plantuml_previewer#plantuml_jar_path"] = "/dev/null"
-      vim.g["plantuml_previewer#save_format"] = "png"
-    end,
   },
 }
