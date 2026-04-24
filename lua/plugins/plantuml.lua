fb56d434079e31493046dcdc046b250485d31e88
Ioannis Canellos <iocanel@gmail.com>
2026-04-24 10:18:21 +0300

    feat(plantuml): add support for auto plantuml compilation and preview

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -2,5 +2,32 @@ return {
   {
     "aklt/plantuml-syntax",
     ft = "plantuml",
+    config = function()
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = { "*.puml", "*.plantuml", "*.pu", "*.uml", "*.iuml" },
+        callback = function()
+          local src = vim.fn.expand("%:p")
+          local dir = vim.fn.expand("%:p:h")
+          vim.fn.jobstart({ "plantuml", "-tsvg", "-o", dir, src }, {
+            stderr_buffered = true,
+            on_stderr = function(_, data)
+              local msg = table.concat(data, "\n"):gsub("^%s+", ""):gsub("%s+$", "")
+              if msg ~= "" then
+                vim.schedule(function()
+                  vim.notify("PlantUML error:\n" .. msg, vim.log.levels.ERROR)
+                end)
+              end
+            end,
+            on_exit = function(_, code)
+              if code == 0 then
+                vim.schedule(function()
+                  vim.notify("PlantUML: diagram updated", vim.log.levels.INFO)
+                end)
+              end
+            end,
+          })
+        end,
+      })
+    end,
   },
 }
